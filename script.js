8756840cc425579bbd8b07b5b6c66fe482de2468
Jishllg Laptop <jishllg@gmail.com>
2020-10-02 00:23:04 -0500

    Added Momentjs functionality.

--- script.js
+++ script.js
@@ -3,20 +3,25 @@ var notes = JSON.parse(localStorage.getItem("notes"));
 if (notes == null) {
     notes = ["", "", "", "", "", "", "", "", ""]
 }
-// Current moment and iterated time which will count up by the hour
+// Current moment and a time moment that will iterate thoughout the program
 var now = moment();
-var time;
+var time = moment();
+time.startOf('day');
 // Integer to deterimine state (past, present, or future) and an array with matching values
 var state = 0;
 var states = ["past", "present", "future"];
 // Array of hour labels to iterate over
 var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
-if (moment('22:00:00', 'hh:mm:ss').isBefore('23:00:00', 'hh:mm:ss')) {
-    state = 2
-};
-// Generate hour blocks
+// Sets the text for the current day
+$("#currentDay").text(now.format('MMMM D, YYYY'))
+// Checks if the current time is before 9AM
+if (now.isBefore(time.add(9, "h"))) {state = 2;}
+// Generate hour blocks 
 hours.forEach(function(hour, i) {
+    // Checks if the current block is in the past, present, or future relative to the current time.
+    time.add(1, "h")
+    if (state < 2 && now.isBefore(time)) {state++;}
     // Creates HTML structure for each hour
     $(".container").append($('<div ' + hour + ' class="row time-block">' +
     '<p class="col-1 hour">' + hour + '</p>' +
